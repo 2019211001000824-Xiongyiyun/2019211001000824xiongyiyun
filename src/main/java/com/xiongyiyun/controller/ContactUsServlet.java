0fe9b9128d1b0995ad37ad6f725328343c34b685
2019211001000824-Xiongyiyun <1587022340@qq.com>
2021-06-14 20:10:57 +0800

    Week-15 Commit

--- src/main/java/com/xiongyiyun/controller/ContactUsServlet.java
+++ src/main/java/com/xiongyiyun/controller/ContactUsServlet.java
@@ -7,13 +7,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "HomeServlet",value="/home")
-public class HomeServlet extends HttpServlet {
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-    }
+@WebServlet("/contactUs")
+public class ContactUsServlet extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("WEB-INF/views/index.jsp").forward(request,response);
+        String path="/WEB-INF/views/contactUs.jsp";
+        request.getRequestDispatcher(path).forward(request,response);
     }
 }
